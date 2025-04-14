e637c737727f198a483b5f0f4073ba4662cedfab
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2025-04-14 15:56:31 +0200

    🌴

--- estampes-id-verification/id.py
+++ estampes-id-verification/id.py
@@ -1,12 +1,16 @@
 from pprint import pprint
 
-with open('id-bd.txt', 'r') as file:
+with open('id_dans_feuille.txt', 'r') as file:
     inbd = file.readlines()
     inbd = [l.replace('\n', '') for l in inbd]
 
-with open('id-fi.txt', 'r') as file:
+with open('id_dans_noms_de_fichiers.txt', 'r') as file:
     infi = file.readlines()
     infi = [l.replace('\n', '') for l in infi]
+
+with open('id_articles_transcrits.txt', 'r') as file:
+    id_articles_transcrits = file.readlines()
+    id_articles_transcrits = [l.replace('\n', '') for l in id_articles_transcrits]
 
 only_in_bd = []
 only_in_filenames = []
@@ -15,12 +19,12 @@ for x in inbd:
     if x not in infi:
         only_in_bd.append(x)
 
-with open('only_in_bd.txt', 'w') as file:
+with open('id_seulement_dans_feuille.txt', 'w') as file:
     file.write(('\n').join(only_in_bd))
 
 for x in infi:
     if x not in inbd:
         only_in_filenames.append(x)
 
-with open('only_in_filenames.txt', 'w') as file:
+with open('id_seulement_dans_noms_de_fichiers.txt', 'w') as file:
     file.write(('\n').join(only_in_filenames))
