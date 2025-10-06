01cc811319de169192e68c1d5fded02e62b600a1
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2025-10-06 09:35:23 +0200

    🚨

--- xmllint.sh
+++ xmllint.sh
@@ -1,4 +1,8 @@
 rm erreurs-xml.log
-for file in ./tei-edition/*.xml; do
+for file in ./tei-articles/*.xml; do
   xmllint --noout "$file" 2>> erreurs-xml.log
+done
+rm erreurs-po-xml.log
+for file in ./po/*.xml; do
+  xmllint --noout "$file" 2>> erreurs-po-xml.log
 done
